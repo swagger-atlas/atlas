ecbe2deff86ec967f1303cbe984aaa60391943a8
Kush Jain <kush.jain@joshtechnologygroup.com>
2018-08-30 15:46:24 +0530

    [LT-70] Locust command update

--- scripts/locust_runner.py
+++ scripts/locust_runner.py
@@ -6,7 +6,18 @@ from settings.conf import settings
 file_path = os.path.join(settings.BASE_DIR, settings.PROJECT_FOLDER_NAME, settings.PROJECT_NAME, settings.OUTPUT_FOLDER,
                          settings.LOCUST_FILE)
 
-COMMAND = "locust -f {file_path} --host={host}".format(file_path=file_path, host=settings.HOST_URL)
+options = [
+    "-f {file_path}".format(file_path=file_path),
+    "--host={host}".format(host=settings.HOST_URL),
+    "--clients 10",
+    "--hatch-rate 1",
+    "--no-web",
+    "--num-request 100",
+    "--only-summary"
+]
+
+COMMAND = "locust {options}".format(options=" ".join(options))
+
 
 if __name__ == "__main__":
     command = COMMAND.split()
