c1b34ef4721cdf4dde33ebc8f9b3a150e3dd25db
Kush Jain <kush.jain@joshtechnologygroup.com>
2018-10-12 14:51:35 +0530

    [LT-118] Convert dict/objects from Settings and Constants

--- atlas/modules/transformer/k6/setup.py
+++ atlas/modules/transformer/k6/setup.py
@@ -36,8 +36,35 @@ class K6Setup:
             with open(out_file, "w+") as out_stream:
                 out_stream.write(resp.text)
 
-    @staticmethod
-    def convert_python_vars_to_js_exports(python_vars, python_object):
+    def convert_python_value_to_js_value(self, python_value, var_name, stringify=True):
+        js_val = None
+
+        if isinstance(python_value, six.string_types):
+            js_val = f"'{python_value}'" if stringify else python_value
+        elif isinstance(python_value, bool):  # This should be checked before integer
+            js_val = BOOL_MAP[python_value]
+        elif isinstance(python_value, int):
+            js_val = python_value
+        elif isinstance(python_value, (list, tuple)):
+            js_val = list(python_value)
+        elif isinstance(python_value, set):
+            js_val = "new Set({})".format(list(python_value))
+        elif hasattr(python_value, "__call__"):
+            js_val = None  # It is a duck-typed function, and should not be copied over
+        elif isinstance(python_value, dict):
+            js_val = {}
+            for key, value in python_value.items():
+                item_val = self.convert_python_value_to_js_value(value, key, False)
+                if item_val is not None:
+                    js_val[key] = item_val
+        else:
+            # While this may not necessarily be an error, we do not convert it.
+            # This includes any Dict structure for now
+            print(f"No compatible data found - {var_name} {python_value}")
+
+        return js_val
+
+    def get_js_exports(self, python_vars, python_object):
 
         out_data = []
         for item in python_vars:
@@ -48,42 +75,23 @@ class K6Setup:
 
             value = getattr(python_object, item)
 
-            js_val = None
-
-            if isinstance(value, six.string_types):
-                js_val = "'{}'".format(value)
-            elif isinstance(value, bool):       # This should be checked before integer
-                js_val = BOOL_MAP[value]
-            elif isinstance(value, int):
-                js_val = value
-            elif isinstance(value, (list, tuple)):
-                js_val = list(value)
-            elif isinstance(value, set):
-                js_val = "new Set({})".format(list(value))
-            elif hasattr(value, "__call__"):
-                continue        # It is a duck-typed function, and should not be copied over
-            elif isinstance(value, dict):
-                print(f"Ignoring {item}")
-            else:
-                # While this may not necessarily be an error, we do not convert it.
-                # This includes any Dict structure for now
-                print("No compatible data found - {} {}".format(item, value))
+            js_val = self.convert_python_value_to_js_value(value, item)
 
             if js_val is not None:
-                out_data.append("export const {} = {};".format(item, js_val))
+                out_data.append(f"export const {item} = {js_val};")
 
         return out_data
 
     def constants_file(self):
         out_file = os.path.join(self.js_lib_path, "constants.js")
-        out_data = self.convert_python_vars_to_js_exports(dir(constants), constants)
+        out_data = self.get_js_exports(dir(constants), constants)
 
         with open(out_file, "w+") as out_stream:
             out_stream.write("\n".join(out_data) + "\n")
 
     def settings_file(self):
         out_file = os.path.join(self.js_lib_path, "settings.js")
-        out_data = self.convert_python_vars_to_js_exports(dir(settings), settings)
+        out_data = self.get_js_exports(dir(settings), settings)
 
         with open(out_file, "w+") as out_stream:
             out_stream.write("\n".join(out_data) + "\n")
