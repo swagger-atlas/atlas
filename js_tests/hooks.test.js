fdfdd1c0a7cc5c7f9ed829cb3af2374591d3792a
Kush Jain <kush.jain@joshtechnologygroup.com>
2019-03-26 18:30:49 +0530

    [LT-295] Hooks Testing

--- js_tests/hooks.test.js
+++ js_tests/hooks.test.js
@@ -12,8 +12,47 @@ describe('hooks class', () => {
 
         test('first time op_id', () => {
             hook.register("op_id", "func");
-            expect(hook.hooks).toEqual({"op_id": ["func"]})
-        })
+            expect(hook.hooks).toEqual({"op_id": ["func"]});
+        });
+
+        test('op_id exists with new function', () => {
+            hook.register("op_id", "func");
+            hook.register("op_id", "new_func");
+            expect(hook.hooks).toEqual({"op_id": ["func", "new_func"]});
+        });
+
+        test('op_id exists with same function', () => {
+            hook.register("op_id", "func");
+            hook.register("op_id", "func");
+            expect(hook.hooks).toEqual({"op_id": ["func"]});
+        });
 
     });
+
+    describe("call function", () => {
+
+        function addOne(...args) {
+            return args.map(num => num + 1);
+        }
+
+        function double(...args) {
+            return args.map(num => num * 2);
+        }
+
+
+        test("no hooks exist for operation id", () => {
+            expect(hook.call("op_id", 1, 2)).toEqual([1, 2]);
+        });
+
+        test("single hook is associated with op_id", () => {
+            hook.register("op_id", addOne);
+            expect(hook.call("op_id", 1, 2)).toEqual([2, 3]);
+        });
+
+        test("multiple hooks are associated, run after each other in chained fashion", () => {
+            hook.register("op_id", addOne);
+            hook.register("op_id", double);
+            expect(hook.call("op_id", 1, 2)).toEqual([4, 6]);
+        });
+    })
 });
