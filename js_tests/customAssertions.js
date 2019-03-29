f1117ec761a7a85ca757f0b2af48a46466e90dfa
Kush Jain <kush.jain@joshtechnologygroup.com>
2019-03-29 18:07:37 +0530

    [LT-305] Resource Test cases

--- js_tests/customAssertions.js
+++ js_tests/customAssertions.js
@@ -1,3 +1,17 @@
+function isSuperSet(set, subSet) {
+    for (let elem of subSet) {
+        if (!set.has(elem)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isSubSet(set, superSet) {
+    return isSuperSet(superSet, set);
+}
+
+
 expect.extend({
     inBetween(num, low, high) {
         const pass = num >= low && num <= high;
@@ -20,5 +34,19 @@ expect.extend({
             pass: pass,
             message: () => `${key} is not an instance of ${type}`
         }
-    }
+    },
+    isSuperSet(set, subSet) {
+        const pass = isSuperSet(set, subSet);
+        return {
+            pass: pass,
+            message: () => `${set} is not superSet of ${subSet}`
+        }
+    },
+    isSubSet(set, superSet) {
+        const pass = isSubSet(set, superSet);
+        return {
+            pass: pass,
+            message: () => `${set} is not sub-set of ${superSet}`
+        }
+    },
 });
